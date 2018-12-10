36dc0cc8738012ff0c8999aca4d2fb4b3b3d7d6d
MajkelT <michal@tuczynski.eu>
2018-12-10 20:32:54 +0100

    git push

---
+++ Pliki/Zadanie#4.py
@@ -0,0 +1,18 @@
+import sys
+import os
+
+def tree(katalog, ile_wciec = 0):
+    zawartosc = list(os.scandir(katalog))
+    for elem in os.scandir(katalog):
+        #print(f"{ile_wciec * '-'}{elem.name}")
+        if elem.name == zawartosc [-1]:
+            print(ile_wciec * '˪   ', elem.name, sep="")
+        else:
+            print(ile_wciec * '|   ', elem.name, sep = "")  #to to samo co wyzej
+        if elem.is_dir():
+            tree(elem, ile_wciec + 1)
+
+
+
+directory = sys.argv[1]
+tree(directory)
